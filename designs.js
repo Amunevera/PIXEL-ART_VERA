3da4612a2e0a72c39bcd984b399336c6c9df55e7
Amunevera <101633114+Amunevera@users.noreply.github.com>
2022-06-28 04:54:56 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,47 +1,58 @@
-var height, width;
+//parameters to use
+//using the getElement to bring out the functionality of the parameter
 
-let submit = document.getElementById('#submit');
-$(document).ready(function() {
-sizePicker =document.addEventListener('submit', function event() {
- //    
-    height=$('#inputHeight').val();
-    width=$('#inputWidth').val();
-    MakeGrid (height, width);
+let sizePicker = document.getElementById('sizePicker');
+let pixelCanvas = document.getElementById("pixelCanvas");
+let submit = document.getElementById('submit');
 
-    sizePicker = function(event) {
-        event.preventDefault();
-        clearGrid();
-        makeGrid();
-    };
-    
+//declaring the global variables
+function createGrid(event) {
+	//call to prevent Default from running while running code
+	event.preventDefault();
+	clearGrid();
+	let height =  document.getElementById('inputHeight');
+	let width = document.getElementById('inputWidth');
+	makeGrid(height, width);
 }
-)}
-)
 
-//call function to create grid
-function MakeGrid(height, width) {
-    for(var i=1; i<=height; i++) {
-        $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
-        for(var k=1; k<=width; k++) {
-            $('#table' + i).append('<td></td>');
-        }
-    }
-};
+//function command to makeGrid
+/**
+ * @desc create a grid of squares 
+ * @param int $width - number of squares representing the width of the grid
+ * @param int $height - number of squares representing the height of the grid
+ */
+function makeGrid(height, width) {
+	//for loops used to createGrid
+	for (let j = 1; j <= height.value; j++) {
+		const tr = document.createElement('tr');
+		tr.setAttribute('id', 'table' + j);
+		pixelCanvas.appendChild(tr);
+		for (let k = 1; k <= width.value; k++) {
+			const td = document.createElement('td');
+			document.getElementById('table' + j).appendChild(td);
+
+		}
+	}
+
+}
+//add event listener to listen for the submit call
+submit.addEventListener('click', createGrid);
 
 //function call to clear existing grid
 function clearGrid() {
-    while (canvas.firstChild) {
-        canvas.removeChild(canvas.firstChild);
-    }
-};
+	while (pixelCanvas.firstChild) {
+		pixelCanvas.removeChild(pixelCanvas.firstChild);
+	}
+}
 
- //to add color to cell
- let color = document.getElementById('#colorPicker');
-pixelCanvas= document.addEventListener('click', function addColor() {
-    color= $('#colorPicker').val();
-        if($(this).attr("style")) {
-            $(this).removeAttr("style")
-        } else {
-            $(this).attr("style", 'background-color:' + color);
-        }
+//to add color to the Grid
+//add eventListener to the pixelCanvas to listen for the addColor command
+pixelCanvas.addEventListener("click", function addColor(evt) {
+	console.log(evt.target);
+	let sizeColor = document.getElementById('colorPicker').value;
+	console.log(sizeColor);
+	if (evt.target.nodeName === "TD") {
+		evt.target.style.backgroundColor = sizeColor;
+	}
+
 });
